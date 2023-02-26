e5874c355d06b85b93230b6f346c094b3527578d
BILLY Ronico <ronico.billy@akka.eu>
2023-02-26 12:20:01 +0100

    test deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,2 @@
-const App = (): JSX.Element => <div>My app</div>
-
+const App = (): JSX.Element => <div>My app is the best!</div>
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,20 @@
+import { initializeApp } from 'firebase/app'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD0LApjVOmTZ4PEtXcreqCHvD6gnimj0Ds',
+  authDomain: 'image-uploader-86053.firebaseapp.com',
+  projectId: 'image-uploader-86053',
+  storageBucket: 'image-uploader-86053.appspot.com',
+  messagingSenderId: '1060588979393',
+  appId: '1:1060588979393:web:daa16db3127dd0e8644040',
+  measurementId: 'G-QC5CL8DDJS',
+}
+
+initializeApp(firebaseConfig)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
