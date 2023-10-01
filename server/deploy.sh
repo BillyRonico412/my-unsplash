4e851e8f9b07b082733352927afe3eedc7dd412b
RonicoBillly <billyronico412@gmail.com>
2023-10-01 21:27:04 +0200

    add pipeline

--- server/deploy.sh
+++
@@ -1,3 +0,0 @@
-sudo docker stop my-unsplash || true && sudo docker rm my-unsplash || true &&
-sudo docker image build -t my-unsplash . &&
-sudo docker container run --name my-unsplash -d -p 3000:8000 my-unsplash
