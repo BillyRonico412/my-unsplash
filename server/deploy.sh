645b6dbd286db5a9591728e485ad9e16c7191c83
RonicoBillly <billyronico412@gmail.com>
2023-10-01 21:09:49 +0200

    test with ls

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,4 +1,3 @@
-sudo docker stop my-unsplash &&
-sudo docker rm my-unsplash &&
+sudo docker stop my-unsplash || true && sudo docker rm my-unsplash || true &&
 sudo docker image build -t my-unsplash . &&
 sudo docker container run --name my-unsplash -d -p 3000:8000 my-unsplash
